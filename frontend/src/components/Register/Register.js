c536365ed268933eb177d6e72cd83de3b6f7ebc2
rohitkrishna094 <rohitkrishna094@gmail.com>
2019-03-24 20:01:01 -0400

    Added state changes for register

--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import './Register.css';
 
 class Register extends Component {
+  state = { username: '', password: '' };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onClick = e => {
+    console.log(this.state.username, this.state.password);
+  };
+
   render() {
     return (
       <div className="register_registerCard">
         <div className="register_heading">USER REGISTER</div>
         <div className="register_formField">
-          <input type="text" placeholder="USERNAME" />
-          <input type="password" placeholder="PASSWORD" />
+          <input
+            type="text"
+            placeholder="USERNAME"
+            name="username"
+            value={this.state.username}
+            onChange={e => this.onChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="PASSWORD"
+            name="password"
+            value={this.state.password}
+            onChange={e => this.onChange(e)}
+          />
         </div>
-        <button className="hvr-fade hvr-bob register_button">register</button>
+        <button className="hvr-fade hvr-bob register_button" onClick={e => this.onClick(e)}>
+          register
+        </button>
         <span className="register_error" hidden>
           errors
         </span>
